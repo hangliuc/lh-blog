4403d7dba3edf090ab7a85e5e358a4fad10df602
liuhang <git.ushareit.me>
2025-09-14 21:14:46 +0800

    更新一些基础配置

--- docs/.vuepress/config/themeConfig/nav.js
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -1,9 +1,9 @@
 // nav
 module.exports = [
   { text: '首页', link: '/' },
-  { text: 'one',link: '/one/',},
-  { text: 'two',link: '/two/',},
-  { text: 'three', link: '/three/' },
+  { text: '运维',link: '/运维/',},
+  { text: '编程',link: '/编程/',},
+  { text: '理财', link: '/理财/' },
   { text: '留言板', link: '/message-board/' },
   { text: '我的博客', link: 'https://wiki.eryajf.net' },
 ]
