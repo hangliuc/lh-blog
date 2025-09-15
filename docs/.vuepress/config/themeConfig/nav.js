98f5857741dae8ad94a6a09f05a323fd8ef8cda6
liuhang <git.ushareit.me>
2025-09-15 14:11:49 +0800

    增加二级目录

--- docs/.vuepress/config/themeConfig/nav.js
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -2,6 +2,7 @@
 module.exports = [
   { text: '首页', link: '/' },
   { text: '运维',link: '/运维/',},
+  { text: 'docker',link: '/运维/docker',},
   { text: '编程',link: '/编程/',},
   { text: '理财', link: '/理财/' },
   { text: '留言板', link: '/message-board/' },
